7f9351c668cb2efd6161858a6f8ddf2964bf035b
LKZ2022 <3251626446@qq.com>
2026-02-03 01:13:53 +0800

    Completed the encapsulation of the ThreadPool class

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <../src/utils/include/ThreadPool.hpp>
+
+std::mutex mtx;
+
+void testPrint(int msg) {
+    mtx.lock();
+    std::cout << "Thread ID: " << std::this_thread::get_id() <<" executing operation output to console, output value: " << msg << std::endl;
+    mtx.unlock();
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ref_storage::utils::ThreadPool  thread_pool(10);
+
+    for (int i = 0; i < 10; i++) {
+        thread_pool.enqueue([i]() {testPrint(i);});
+    }
+
     return 0;
+
 }
--- src/utils/include/ThreadPool.hpp
+++ src/utils/include/ThreadPool.hpp
@@ -27,7 +27,31 @@ namespace ref_storage::utils {
 
         // Submit Task.
         template <class F, class... Args>
-        auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
+        auto enqueue(F&& f, Args&&... args) -> std::future<typename std::invoke_result<F, Args...>::type> {
+
+            using return_type = typename std::invoke_result<F, Args...>::type;
+
+            auto task = std::make_shared< std::packaged_task<return_type()> >(
+                [func = std::forward<F>(f), args = std::make_tuple(std::forward<Args>(args)...)]() mutable {
+                    return std::apply(func, std::move(args));
+                }
+            );
+
+            std::future<return_type> res = task->get_future();
+            {
+                std::unique_lock<std::mutex> lock(queue_mutex);
+
+                if (stop) {
+                    throw std::runtime_error("enqueue on stopped ThreadPool");
+                }
+                tasks.emplace([task]() {
+                    (*task)();
+                });
+            }
+
+            condition.notify_one();
+            return res;
+        }
     };
 
 
