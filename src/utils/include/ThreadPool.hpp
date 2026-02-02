7ae4c3ef6d0ecbc6d3fb7805268f25822705dd16
LKZ2022 <3251626446@qq.com>
2026-02-03 00:20:21 +0800

    Completed the encapsulation of the ThreadPool class

--- src/utils/include/ThreadPool.hpp
+++ src/utils/include/ThreadPool.hpp
@@ -2,10 +2,33 @@
 //Licensed under the Apache License, Version 2.0.
 
 #pragma once
+#include <condition_variable>
+#include <thread>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <future>
+#include <mutex>
 
 namespace ref_storage::utils {
 
     class ThreadPool {
+    private:
+        std::vector<std::thread> workers;
+        std::queue<std::function<void()> > tasks;
+
+        std::mutex queue_mutex;
+        std::condition_variable condition;
+        bool stop = false;
+
+    public:
+        explicit ThreadPool(size_t threads);
+        ~ThreadPool();
+
+        // Submit Task.
+        template <class F, class... Args>
+        auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
     };
+
 
 }
